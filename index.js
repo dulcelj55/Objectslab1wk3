7a3a59d351d0013e43be4808a161fb0bdb68f1a3
Ducle Jaramillo <djaramillo@LABA.PSWAS.LOCAL>
2023-08-16 15:44:19 -0700

    finsihed work

---
+++ index.js
@@ -0,0 +1,119 @@
+// Problem 1
+
+// const carThree={
+//   wheels: 4,
+//   owner: "Donald",
+//   isBroken: false,
+//   color: "red"
+// }
+
+// carThree.price=1400;
+// console.log(carThree.price);
+// carThree ['year'] = 2019;
+// console.log(carThree["year"]);
+// carThree.isBroken= true;
+// console.log(carThree.isBroken);
+// console.log (carThree.owner);
+
+
+//  let clientIsInterested;
+
+// if(carThree.color === "blue"){
+//   clientIsInterested = true
+// } else{
+//   clientIsInterested = false
+// }
+// delete carThree.owner;
+// console.log(clientIsInterested);
+// console.log(carThree);
+
+// Problem 2
+// const store = {
+//   profit: 120,
+//   manager: "Bob",
+//   isOpenNow: true,
+//   isVeryExpensive: false
+// }
+
+// store.location= "cap hill";
+// console.log(store.location);
+// store['storeType']= "busy";
+// console.log(store.storeType);
+// store.isOpenNow = false;
+// console.log(store.isOpenNow);
+// console.log(store.isVeryExpensive);
+// if (store.isOpenNow === true && store.isVeryExpensive === false ){
+//   canShopHere = true;
+// } else{
+//   canShopHere = false;   
+// }
+// delete store.profit;
+// console.log(store);
+
+// Problem 3
+// const home = {
+//   rooms: 20,
+//   bathrooms: 3,
+//   isForSale: true,
+//   isInGoodLocation: false,
+//   }
+
+//   home.price = "1 million";
+//   console.log(home.price);
+//   home["year"] = 1999;
+//   console.log (home.year);
+//   home.value = "1 dollar";
+//   console.log( home.value);
+//   console.log(home.bathrooms);
+
+//   let buyHouse
+//   if (home.isForSale === true && home.isInGoodLocation){
+//     buyHouse = true
+//   }else{
+//     buyhouse = false
+//   }
+//   console.log(buyhouse);
+// Problem 4
+// const chula = {
+//   age: 7,
+//   size: "medium",
+//   treat: "chicken",
+//   exercise: "lots"
+// }
+// chula.favToy = "ball";
+// chula ["bday"] = 8.2016;
+// chula. size = "large";
+// console.log(chula.exercise);
+
+// let hasFriends
+
+// if (chula.exercise === "lots" && chula.favToy === "ball"){
+//   sheCanPlay = true 
+// }else{
+//   shecanPlay = false
+// }
+// console.log(chula);
+// console.log(sheCanPlay);
+
+const candyshop = {
+  moneymaker: true,
+  milkshakes: "$4",
+  customers: 45,
+  wallColors: "pink"
+}
+candyshop.chocolate = "yes"
+console.log(candyshop.chocolate);
+candyshop["insurance"] = "no";
+console.log(candyshop.insurance);
+candyshop.chocolate = "no"
+console.log(candyshop.chocolate)
+if (candyshop.chocolate == "no" && candyshop.insurance == "no") {
+  doTrust = false
+} else doTrust = true;
+console.log (candyshop);
+console.log (doTrust);
+
+
+
+
+
